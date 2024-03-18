897448fa37786043d69e0b5c8f27d22db1a583a9
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-03-18 22:45:40 +0200

    fix nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import logo from '../public/logo.png'
+import chrome from '../public/chrome.png'
 import { NAV_LINKS } from '@/constants' 
 import ImageUse from './Image'
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-between padding-container  py-5 absolute inset-x-0 top-0 z-50'>
+    <nav className='flex justify-between padding-container  pt-12 absolute inset-x-0 top-0 z-50'>
         <Link href='/'>
             <ImageUse
               src={logo}
@@ -23,6 +24,18 @@ const Navbar = () => {
                 <Link href={link.href} key={link.key} className='text-[#090337] transition-all px-2 py-0.5 cursor-pointer rounded font-medium'>{link.label}</Link>
             ))}
         </ul>
+        <div className='flex gap-3.5 items-center'>
+          <Link href='/'>Login</Link>
+          <div className='flex relative'>
+            <p
+              className='text-[#090337] w-full absolute -top-10 right-0  text-sm font-bold py-3 px-6 rounded-lg cursor-pointer'
+            >100% free, forever 🤑</p>
+            <button className='flex gap-2 bg-[#18181C] font-bold py-3 px-6 rounded-lg  items-center'>
+              <Image src={chrome} height={19} width={19} alt="logo" />
+              <span className='text-white mulish-font text-sm'>Install Wizardshot</span>
+              </button>
+          </div>
+        </div>
     </nav>
   )
 }
