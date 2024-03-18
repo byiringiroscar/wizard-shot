b146396549a72186f89c9547562b734d93bf377c
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-03-18 09:58:12 +0200

    wip on strucuture

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      screens: {
+        'xs': '400px',
+        '3xl': '1680px',
+        '4xl': '2200px',
       },
     },
   },
