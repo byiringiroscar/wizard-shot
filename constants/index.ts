fa3f423c20011c07a0fe579207825923f45a35d8
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-03-18 10:04:05 +0200

    wip on nav

---
+++ constants/index.ts
@@ -0,0 +1,7 @@
+// NAVIGATION
+export const NAV_LINKS = [
+    { href: '/', key: 'home', label: 'Home' },
+    { href: '/features', key: 'Features', label: 'Features' },
+    { href: '/about', key: 'About', label: 'About' },
+    { href: '/help', key: 'help ', label: 'Help Center & Community ' },
+  ];
