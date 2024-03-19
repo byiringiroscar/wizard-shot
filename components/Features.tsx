0aef95689ae8a0e6467fce3a7e5e1bec01a5c173
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-03-19 11:47:38 +0200

    Create Features.tsx

---
+++ components/Features.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Features = () => {
+  return (
+    <div className='main-container flex flex-col items-center'>
+        <h3><span className='underline'>32 MORE FEATURES</span> <br><span>INCLUDING A KB INTEGRATION</span></br></h3>
+    </div>
+  )
+}
+
+export default Features
