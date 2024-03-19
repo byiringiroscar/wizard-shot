d1a2337c485a1c26fcd0eb34ddff57a432cb048b
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-03-19 11:18:09 +0200

    wip on strucutre

--- components/Process.tsx
+++ components/Process.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import ProcessCard from "./ProcessCard"
+import { processData } from "@/dataMock/data"
 
 const Process = () => {
   return (
-    <section className='max-container padding-container pt-8'>
+    <section className='pt-8 lg:px-8'>
         <h2 className='text-[32px] leading-[35px] text-center lg:text-[40px] lg:leading-[48px] font-bold jakarta-font text-gray-900'>Say <span className='underline'>Goodbye To Writing Docs</span>, and Hello To Wizardshot Doing it for YOU.</h2>
-        <div className='grid grid-cols-1 py-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 pt-4 lg:pt-8'>
-            
+        <div className='grid grid-cols-1 py-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-5 pt-4 lg:pt-8'>
+            {processData.map((process, index) => (
+                <ProcessCard key={index} step={process.step} image={process.image} title={process.title} description={process.description} />
+            ))}
+
         </div>
     </section>
   )
---
+++ components/ProcessCard.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Image from 'next/image'
+
+type ProcessInterface = {
+    step: number,
+    image: any,
+    title: string,
+    description: string
+}
+
+const ProcessCard = ({step, image, title, description}: ProcessInterface) => {
+  return (
+    <div className='flex flex-col gap-3'>
+        <Image src={image}
+        alt='img'
+        height={100}
+        width={100}
+        className='w-full h-[230px]'
+            />
+        <div className='flex flex-col gap-1'>
+            <h3 className='text-[18px] leading-[25px] font-bold opensans-font text-[#363636]'>{step}. {title}</h3>
+            <p className='text-[14px] leading-[16px] opensans-font-regular text-[#000000]'>{description}</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default ProcessCard
